8dcd61828134920ca921cf6b84a81fdacadeaf66
eleos <30459680+ELEOS-MAP@users.noreply.github.com>
2019-08-05 21:37:33 +0100

    first commit
    fixall

---
+++ backend/build/precache-manifest.29f18e96209ef1f8a38140876c65d4ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17cae26d2f8ef965d0563b768f4dc6b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c915795d970c50113139",
+    "url": "/static/css/main.fb7da985.chunk.css"
+  },
+  {
+    "revision": "ab6217add79d48b2a0e1",
+    "url": "/static/js/2.b886e112.chunk.js"
+  },
+  {
+    "revision": "c915795d970c50113139",
+    "url": "/static/js/main.c1d0967c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "80b823962530db98c76fdaac6f0781a5",
+    "url": "/static/media/FAlogo.80b82396.png"
+  },
+  {
+    "revision": "e2f2b8d21342432eea3acb12f678d5ef",
+    "url": "/static/media/land.e2f2b8d2.png"
+  }
+]);
